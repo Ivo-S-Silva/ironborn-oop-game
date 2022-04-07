3627c00acb76a04251a5106c62bdae13606a97dd
Ivo Silva <silvaivosoares@gmail.com>
2022-04-07 14:26:25 +0100

    Added the player and gave it move functionality

--- js/game.js
+++ js/game.js
@@ -1,1 +1,61 @@
-console.log("Hello World");
+/*
+Player:
+
+Info:
+- Position
+
+
+Functionality:
+- moveLeft()
+- moveRight()
+*/
+
+class Game {
+    constructor(createElement, drawElement){
+        this.player = null;
+        // It has the function createDomElement inside this property, passed to the constructor
+        this.createElement = createElement;
+        this.drawElement = drawElement;
+    }
+
+    start(){
+        this.player = new Player();
+        this.player.domElement = this.createElement("player");
+        this.drawElement(this.player);
+    }
+
+    movePlayer(direction){
+        switch(direction){
+            case "left":
+                this.player.moveLeft();
+                break;
+            case "right":
+                this.player.moveRight();
+                break;
+        }
+        // Each time the player moves, the element is redrawn
+        // Calls the drawElement function and says it's the player
+        // that is moving.
+        this.drawElement(this.player);
+    }
+}
+
+class Player {
+    constructor(){
+        this.positionX = 45;
+        this.positionY = 0;
+        this.domElement = null;
+    }
+
+    moveLeft(){
+        // Moving to the left along the X axis. Decreasing the value of the X axis
+        this.positionX -=2;
+    }    
+
+    moveRight(){
+        this.positionX +=2;
+    }
+}
+
+
+const myPlayer = new Player();
---
+++ js/main.js
@@ -0,0 +1,38 @@
+function createDomElement(className){
+    const board = document.getElementById("board");
+    const newElement = document.createElement("div");
+    
+    //Identifying the specific type of element we are creating (player, enemy, wall)
+    newElement.className = className;
+
+    board.appendChild(newElement);
+
+    return newElement;
+}
+
+// Create a function to move a DOM element. instance is passed to identify
+// the element to move
+function drawDomElement(instance){    
+    instance.domElement.style.left  = instance.positionX + "%";
+    instance.domElement.style.bottom  = instance.positionY + "%";
+}
+
+// Starting a new game by creating a new instance of the game class
+// Passing the createDomElement function to be able to execute it at game start
+const game = new Game(createDomElement, drawDomElement);
+game.start();
+
+document.addEventListener('keydown', function(event){
+/*Every time an event is fired, you can get the information about it in a variable 
+that can be passed inside the function of the event. Using developer tools it's possible to
+see all the properties it has to be able to interact with it.*/
+
+switch(event.key){
+    case "ArrowRight":
+        game.movePlayer("right");
+        break;
+    case "ArrowLeft":
+        game.movePlayer("left");
+        break;
+}
+})
